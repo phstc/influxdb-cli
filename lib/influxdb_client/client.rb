5647fb9d4ef1ceab3d1c85635b0e728f65f96d1d
Pablo Cantero <pablo@pablocantero.com>
2013-12-15 11:22:52 -0200

    Moves print_table to InfluxDBClient::Client

--- lib/influxdb_client/client.rb
+++ lib/influxdb_client/client.rb
@@ -1,6 +1,23 @@
+require 'terminal-table'
+
 module InfluxDBClient
   class Client
     QUERY_LANGUAGE_MATCHER = /\A\s*((delete\s+from|select\s+.+\s+from)\s.+)\z/i
+
+    def self.print_tabularize(result, output=$stdout)
+      number_of_points = {}
+      result.to_h.keys.each do |series|
+        headings = result[series].first.keys
+        rows = result[series].collect(&:values)
+        table = Terminal::Table.new title: series, headings: headings, rows: rows
+        output.puts table
+        # empty line between time series output
+        output.puts
+        # count total per time series
+        number_of_points["#{series}.count"] = result[series].size
+      end
+      number_of_points
+    end
   end
 end
 
--- spec/influxdb_client/client_spec.rb
+++ spec/influxdb_client/client_spec.rb
@@ -24,6 +24,39 @@ module InfluxDBClient
         it { should_not match('DELETE * FROM series1, series2') }
       end
     end
+
+    describe '.print_tabularize' do
+      let(:result)  { { series1: [{ value1: 1, value2: 2 }],
+                        series2: [{ value3: 3,   value4: 4, value5: nil, value6: nil },
+                                  { value3: nil, value4: 4, value5: 5,   value6: 6   }] } }
+
+      it 'returns series count' do
+        expect(described_class.print_tabularize(result)).to eq({ 'series1.count' => 1, 'series2.count' => 2 })
+      end
+
+      it 'generates tables' do
+        expect(Terminal::Table).to receive(:new).
+          with(title: :series1, headings: [:value1, :value2], rows: [[1, 2]])
+
+        expect(Terminal::Table).to receive(:new).
+          with(title: :series2, headings: [:value3, :value4, :value5, :value6], rows: [[3, 4, nil, nil], [nil, 4, 5, 6]])
+
+        described_class.print_tabularize(result)
+      end
+
+      it 'puts tables' do
+        output = double 'Output'
+        table  = double 'Table'
+        Terminal::Table.stub(new: table)
+
+        # should print series1 and series2
+        expect(output).to receive(:puts).twice.with(table)
+        # line break for series
+        expect(output).to receive(:puts).twice.with(no_args)
+
+        described_class.print_tabularize(result, output)
+      end
+    end
   end
 end
 
